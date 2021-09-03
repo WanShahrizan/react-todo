98a5ae2a9b0e8a406ed8dcdcd55b0fc4adcd027a
WanShahrizan <shahrizan_98@yahoo.com>
2021-09-03 19:08:37 +0800

    Add in Redux, Working fine

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,13 +1,26 @@
-const React = require("react");
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 const NewTodoFormTwo = require("./NewTodoForm2");
 const TodoListItem = require("./TodoListItem");
+import { loadTodos } from "./thunks";
 import { removeTodo, markTodoAsCompleted } from "./actions";
+import { isLoading } from "./reducers";
+import { displayAlert } from "./thunks";
 
 import "./TodoList.css";
 
-function TodoList({ todos = [], onRemovePressed, onCompletedPressed }) {
-  return (
+function TodoList({
+  todos = [],
+  onRemovePressed,
+  onCompletedPressed,
+  isLoading,
+  startLoadingTodos,
+}) {
+  useEffect(() => {
+    startLoadingTodos();
+  }, []);
+  const loadingMessage = <div>Loading todos...</div>;
+  const content = (
     <div className="list-wrapper">
       <NewTodoFormTwo />
       {todos.map((todo) => (
@@ -19,13 +32,16 @@ function TodoList({ todos = [], onRemovePressed, onCompletedPressed }) {
       ))}
     </div>
   );
+  return isLoading ? loadingMessage : content;
 }
 
 const mapStateToProps = (state) => ({
+  isLoading: state.isLoading,
   todos: state.todos,
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  startLoadingTodos: () => dispatch(loadTodos()),
   onRemovePressed: (text) => dispatch(removeTodo(text)),
   onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
 });
---
+++ src/todos/thunks.js
@@ -0,0 +1,45 @@
+//A function that return
+//another function that contain
+//the actual logic that we want to perform
+
+import {
+  loadTodosInProgress,
+  loadTodosSuccess,
+  loadTodosFailure,
+  createTodo,
+} from "./actions";
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+
+    const response = await fetch("http://localhost:8080/todos");
+    const todos = await response.json();
+
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+};
+
+export const addTodoRequest = (text) => async (dispatch) => {
+  try {
+    const body = JSON.stringify({ text });
+    const response = await fetch("http://localhost:8080/todos", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body,
+    });
+    const todo = await response.json();
+    dispatch(createTodo(todo));
+  } catch (e) {
+    dispatch(displayAlert(e));
+  }
+};
+
+export const displayAlert = (text) => () => {
+  alert(text);
+};
